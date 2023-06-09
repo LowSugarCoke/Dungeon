161f2abf5073da1a6d781ee99ffb1ee78022efef
Jack Lee <jack20499@gmail.com>
2023-06-10 00:34:38 +0800

    first commit

---
+++ Dungeon/Dungeon.cpp
@@ -0,0 +1,7 @@
+#include "Dungeon.h"
+
+Dungeon::Dungeon(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
---
+++ Dungeon/Dungeon.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_Dungeon.h"
+
+class Dungeon : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	Dungeon(QWidget *parent = Q_NULLPTR);
+
+private:
+	Ui::DungeonClass ui;
+};
