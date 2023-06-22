4604dd286289d0c1b8896187b8d3bc4693af5c33
Jack Lee <jack20499@gmail.com>
2023-06-22 21:41:30 +0800

    refact: add comments in monster

--- Dungeon/monster.cpp
+++ Dungeon/monster.cpp
@@ -12,87 +12,87 @@
 #include <QCoreApplication>
 
 Monster::Monster()
-	: QGraphicsPixmapItem() {
-	setFlag(QGraphicsItem::ItemIsFocusable, true);  // Set item to be focusable
+    : QGraphicsPixmapItem() {
+    setFlag(QGraphicsItem::ItemIsFocusable, true);  // Set item to be focusable
 }
 
 void Monster::setMonsterImg(const QString& kMonsterImg) {
-	QPixmap pixmap(kMonsterImg);  // Change this to your image path
-	setPixmap(pixmap.scaled(QSize(34, 34), Qt::KeepAspectRatio));
+    QPixmap pixmap(kMonsterImg);  // Change this to your image path
+    setPixmap(pixmap.scaled(QSize(34, 34), Qt::KeepAspectRatio));
 }
 
 void Monster::keyPressEvent(QKeyEvent* event) {
-	// Calculate new position
-	QPointF newPos = pos();
-	QPointF beforePos = pos();
-	if (event->key() == Qt::Key_W) {
-		newPos.setY(newPos.y() - brickSize);
-	}
-	else if (event->key() == Qt::Key_S) {
-		newPos.setY(newPos.y() + brickSize);
-	}
-	else if (event->key() == Qt::Key_A) {
-		newPos.setX(newPos.x() - brickSize);
-	}
-	else if (event->key() == Qt::Key_D) {
-		newPos.setX(newPos.x() + brickSize);
-	}
+    // Calculate new position
+    QPointF newPos = pos();
+    QPointF beforePos = pos();
+    if (event->key() == Qt::Key_W) {
+        newPos.setY(newPos.y() - mBrickSize);
+    }
+    else if (event->key() == Qt::Key_S) {
+        newPos.setY(newPos.y() + mBrickSize);
+    }
+    else if (event->key() == Qt::Key_A) {
+        newPos.setX(newPos.x() - mBrickSize);
+    }
+    else if (event->key() == Qt::Key_D) {
+        newPos.setX(newPos.x() + mBrickSize);
+    }
 
-	// Check for collisions
-	setPos(newPos);
-	if (!collidingItems().isEmpty()) {
-		// Check for collisions with the hero after the move
-		QList<QGraphicsItem*> collidingItems = this->collidingItems();
+    // Check for collisions
+    setPos(newPos);
+    if (!collidingItems().isEmpty()) {
+        // Check for collisions with the hero after the move
+        QList<QGraphicsItem*> collidingItems = this->collidingItems();
 
-		for (auto* item : collidingItems) {
-			// Check if the colliding item is the hero
-			auto* hero = dynamic_cast<Hero*>(item);
-			if (hero) {
-				hero->checkCollision();
-				// If colliding with any item, undo move
-			}
+        for (auto* item : collidingItems) {
+            // Check if the colliding item is the hero
+            auto* hero = dynamic_cast<Hero*>(item);
+            if (hero) {
+                hero->checkCollision();
+                // If colliding with any item, undo move
+            }
 
-			auto* collection = dynamic_cast<Collection*>(item);
-			auto* potion = dynamic_cast<Potion*>(item);
-			auto* trap = dynamic_cast<Trap*>(item);
-			auto* superPotion = dynamic_cast<SuperPotion*>(item);
+            auto* collection = dynamic_cast<Collection*>(item);
+            auto* potion = dynamic_cast<Potion*>(item);
+            auto* trap = dynamic_cast<Trap*>(item);
+            auto* superPotion = dynamic_cast<SuperPotion*>(item);
 
-			if (!(collection || potion || trap || superPotion)) {
-				setPos(beforePos);
-				return;
-			}
-		}
-	}
+            if (!(collection || potion || trap || superPotion)) {
+                setPos(beforePos);
+                return;
+            }
+        }
+    }
 
-	QGraphicsItem::keyPressEvent(event);  // Pass the event to the base class
+    QGraphicsItem::keyPressEvent(event);  // Pass the event to the base class
 }
 
 void Monster::setStepSize(const int& kStepSize) {
-	brickSize = kStepSize;
+    mBrickSize = kStepSize;
 }
 
 void Monster::randomMove() {
-	// Choose a random direction for the monster to move in
-	int direction = QRandomGenerator::global()->bounded(4);
-	QKeyEvent* event;
-	switch (direction) {
-	case 0:
-		event = new QKeyEvent(QEvent::KeyPress, Qt::Key_W, Qt::NoModifier);
-		break;
-	case 1:
-		event = new QKeyEvent(QEvent::KeyPress, Qt::Key_S, Qt::NoModifier);
-		break;
-	case 2:
-		event = new QKeyEvent(QEvent::KeyPress, Qt::Key_A, Qt::NoModifier);
-		break;
-	case 3:
-		event = new QKeyEvent(QEvent::KeyPress, Qt::Key_D, Qt::NoModifier);
-		break;
-	default:
-		return;
-	}
+    // Choose a random direction for the monster to move in
+    int direction = QRandomGenerator::global()->bounded(4);
+    QKeyEvent* event;
+    switch (direction) {
+    case 0:
+        event = new QKeyEvent(QEvent::KeyPress, Qt::Key_W, Qt::NoModifier);
+        break;
+    case 1:
+        event = new QKeyEvent(QEvent::KeyPress, Qt::Key_S, Qt::NoModifier);
+        break;
+    case 2:
+        event = new QKeyEvent(QEvent::KeyPress, Qt::Key_A, Qt::NoModifier);
+        break;
+    case 3:
+        event = new QKeyEvent(QEvent::KeyPress, Qt::Key_D, Qt::NoModifier);
+        break;
+    default:
+        return;
+    }
 
-	// Pass the event to keyPressEvent function
-	keyPressEvent(event);
-	delete event;  // Don't forget to delete the event after we are done
+    // Pass the event to keyPressEvent function
+    keyPressEvent(event);
+    delete event;  // Don't forget to delete the event after we are done
 }
--- Dungeon/monster.h
+++ Dungeon/monster.h
@@ -1,20 +1,46 @@
+/**
+ * @file monster.h
+ * @author Jack Lee
+ * @date 2023.06.22
+ *
+ * @brief This header file contains the Monster class, representing a monster in the game.
+ */
+
 #include <QObject>
 #include <QGraphicsPixmapItem>
 
 class Monster : public QObject, public QGraphicsPixmapItem {
-    Q_OBJECT
+    Q_OBJECT // This macro is needed for any object that uses Qt's signal-slot system
 
 public:
+    /**
+     * @brief Constructor. It initializes the Monster object.
+     */
     Monster();
-    virtual ~Monster() {}
 
+    /**
+     * @brief Sets the image of the monster.
+     * @param kMonsterImg A constant reference to a QString object that contains the image file's path.
+     */
     void setMonsterImg(const QString& kMonsterImg);
+
+    /**
+     * @brief Handles the key press event.
+     * @param event A pointer to the QKeyEvent object that describes the event.
+     */
     void keyPressEvent(QKeyEvent* event);
+
+    /**
+     * @brief Sets the step size of the monster.
+     * @param kStepSize A constant reference to an integer specifying the step size.
+     */
     void setStepSize(const int& kStepSize);
+
+    /**
+     * @brief Makes the monster move randomly.
+     */
     void randomMove();
 
-
-
 private:
-    int brickSize;
+    int mBrickSize;  // Size of the bricks or tiles that make up the game's maze
 };
