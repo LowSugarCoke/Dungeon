f8aa8aa436906d20b6a28074819d6a9e1ada28d6
Jack Lee <jack20499@gmail.com>
2023-06-22 22:31:10 +0800

    refact: refactor mainMenuScene button and add comments

--- Dungeon/mainMenuScene.cpp
+++ Dungeon/mainMenuScene.cpp
@@ -10,171 +10,129 @@
 #include "resource.h"
 #include "Dungeon.h"
 
+// Constructor for the MainMenuScene class.
 MainMenuScene::MainMenuScene(QObject* parent)
     : QGraphicsScene(parent)
 {
-    m_backgroundItem = new QGraphicsPixmapItem();
-    addItem(m_backgroundItem);
-    m_opacityEffect = new QGraphicsOpacityEffect();
-    m_opacityEffect->setOpacity(1.0);
-    m_backgroundItem->setGraphicsEffect(m_opacityEffect);
+    // Initialize and set up background item.
+    mBackgroundItem = new QGraphicsPixmapItem();
+    addItem(mBackgroundItem);
+    mOpacityEffect = new QGraphicsOpacityEffect();
+    mOpacityEffect->setOpacity(1.0);
+    mBackgroundItem->setGraphicsEffect(mOpacityEffect);
 
-    m_title = new QGraphicsPixmapItem();
-    addItem(m_title);
-    m_opacityEffect = new QGraphicsOpacityEffect();
-    m_opacityEffect->setOpacity(1.0);
-    m_title->setGraphicsEffect(m_opacityEffect);
+    // Initialize and set up title item.
+    mTitle = new QGraphicsPixmapItem();
+    addItem(mTitle);
+    mOpacityEffect = new QGraphicsOpacityEffect();
+    mOpacityEffect->setOpacity(1.0);
+    mTitle->setGraphicsEffect(mOpacityEffect);
 
+    // Set image for title item.
     QPixmap backgroundImage(UIResource::kTitle);
-    m_title->setPixmap(backgroundImage);
-    m_title->setPos(50, 50);
+    mTitle->setPixmap(backgroundImage);
+    mTitle->setPos(50, 50);
 
+    // Setup layout and root widget.
     QGraphicsWidget* root = new QGraphicsWidget();
     QGraphicsLinearLayout* layout = new QGraphicsLinearLayout(Qt::Vertical, root);
     root->setLayout(layout);
     root->resize(1920, 1080);
     addItem(root);
-
     root->setPos(700, 300);
 
-    startButton = new QPushButton(QString::fromLocal8Bit("開始遊戲"));
-    startButton->setStyleSheet(R"(
-    QPushButton {
-        font-size:20px;
-        font-weight:bold;
-        background:none;
-        background-color:transparent;
-        border-image: url(Resources/img/button.png) 0 0 0 0 stretch stretch;
-    }
-    QPushButton:hover {
-        color:white;
-    }
-    QPushButton:pressed {
-        color:black;
-    }
-)");
+    // Setup the start button and its properties.
+    mStartButton = setupButton(QString::fromLocal8Bit("開始遊戲"), layout);
+    connect(mStartButton, &QPushButton::clicked, this, &MainMenuScene::handleStartButton);
 
-    startButton->setMinimumSize(400, 120);
-    startButton->setMaximumSize(400, 120);
-    QGraphicsProxyWidget* startButtonProxy = addWidget(startButton);
-    //startButtonProxy->resize(100, 30);
-    layout->addItem(startButtonProxy);
-    layout->setAlignment(startButtonProxy, Qt::AlignCenter);  // 設定對齊方式
-    connect(startButton, &QPushButton::clicked, this, &MainMenuScene::handleStartButton);
+    // Setup the loading button and its properties.
+    mLoadingButton = setupButton(QString::fromLocal8Bit("讀取存檔"), layout);
+    connect(mLoadingButton, &QPushButton::clicked, this, &MainMenuScene::handleLoadingButton);
 
-    loadingButton = new QPushButton(QString::fromLocal8Bit("讀取存檔"));
-    loadingButton->setStyleSheet(R"(
-    QPushButton {
-        font-size:20px;
-        font-weight:bold;
-        background:none;
-        background-color:transparent;
-        border-image: url(Resources/img/button.png) 0 0 0 0 stretch stretch;
-    }
-    QPushButton:hover {
-        color:white;
-    }
-    QPushButton:pressed {
-        color:black;
-    }
-)");
+    // Setup the settings button and its properties.
+    mSettingsButton = setupButton(QString::fromLocal8Bit("設定"), layout);
+    connect(mSettingsButton, &QPushButton::clicked, this, &MainMenuScene::handleSettingsButton);
 
-    loadingButton->setMinimumSize(400, 120);
-    loadingButton->setMaximumSize(400, 120);
-    QGraphicsProxyWidget* loadinguttonProxy = addWidget(loadingButton);
-    //startButtonProxy->resize(100, 30);
-    layout->addItem(loadinguttonProxy);
-    layout->setAlignment(loadinguttonProxy, Qt::AlignCenter);  // 設定對齊方式
-    connect(loadingButton, &QPushButton::clicked, this, &MainMenuScene::handleLoadingButton);
-
-    settingsButton = new QPushButton(QString::fromLocal8Bit("設定"));
-    settingsButton->setStyleSheet(R"(
-    QPushButton {
-        font-size:20px;
-        font-weight:bold;
-        background:none;
-        background-color:transparent;
-        border-image: url(Resources/img/button.png) 0 0 0 0 stretch stretch;
-    }
-   QPushButton:hover {
-        color:white;
-    }
-    QPushButton:pressed {
-        color:black;
-    }
-)");
-    settingsButton->setMinimumSize(400, 120);
-    settingsButton->setMaximumSize(400, 120);
-    QGraphicsProxyWidget* settingsButtonProxy = addWidget(settingsButton);
-    layout->addItem(settingsButtonProxy);
-    layout->setAlignment(settingsButtonProxy, Qt::AlignCenter);  // 設定對齊方式
-    connect(settingsButton, &QPushButton::clicked, this, &MainMenuScene::handleSettingsButton);
-
-    exitButton = new QPushButton(QString::fromLocal8Bit("結束遊戲"));
-    exitButton->setStyleSheet(R"(
-    QPushButton {
-        font-size:20px;
-        font-weight:bold;
-        background:none;
-        background-color:transparent;
-        border-image: url(Resources/img/button.png) 0 0 0 0 stretch stretch;
-    }
-    QPushButton:hover {
-        color:white;
-    }
-    QPushButton:pressed {
-        color:black;
-    }
-)");
-    exitButton->setMinimumSize(400, 120);
-    exitButton->setMaximumSize(400, 120);
-    QGraphicsProxyWidget* exitButtonProxy = addWidget(exitButton);
-    layout->addItem(exitButtonProxy);
-    layout->setAlignment(exitButtonProxy, Qt::AlignCenter);  // 設定對齊方式
-    connect(exitButton, &QPushButton::clicked, this, &MainMenuScene::handleExitButton);
+    // Setup the exit button and its properties.
+    mExitButton = setupButton(QString::fromLocal8Bit("結束遊戲"), layout);
+    connect(mExitButton, &QPushButton::clicked, this, &MainMenuScene::handleExitButton);
 }
 
+// Function to handle the start button click.
 void MainMenuScene::handleStartButton() {
     static_cast<Dungeon*>(parent())->intro();
 }
+
+// Function to handle the loading button click.
 void MainMenuScene::handleLoadingButton() {
     static_cast<Dungeon*>(parent())->load();
 }
 
+// Function to handle the settings button click.
 void MainMenuScene::handleSettingsButton() {
-    // 處理當"設定"按鈕被點擊時的邏輯
     static_cast<Dungeon*>(parent())->setting();
 }
 
+// Function to handle the exit button click.
 void MainMenuScene::handleExitButton() {
-    // 處理當"結束遊戲"按鈕被點擊時的邏輯
     QApplication::quit();
 }
 
+// Function to set the image of the scene.
 void MainMenuScene::setSceneImg(const QString& kSceneImg) {
     QPixmap backgroundImage(kSceneImg);
     backgroundImage = backgroundImage.scaled(sceneRect().size().toSize(), Qt::KeepAspectRatioByExpanding);
-    m_backgroundItem->setPixmap(backgroundImage);
+    mBackgroundItem->setPixmap(backgroundImage);
 }
 
+// Function to animate a fade in over the specified duration.
 void MainMenuScene::fadeIn(int duration) {
-    startButton->show();
-    settingsButton->show();
-    exitButton->show();
-    QPropertyAnimation* animation = new QPropertyAnimation(m_opacityEffect, "opacity");
+    mStartButton->show();
+    mSettingsButton->show();
+    mExitButton->show();
+    QPropertyAnimation* animation = new QPropertyAnimation(mOpacityEffect, "opacity");
     animation->setDuration(duration);
     animation->setStartValue(0.0);
     animation->setEndValue(1.0);
     animation->start(QAbstractAnimation::DeleteWhenStopped);
 }
 
+// Function to animate a fade out over the specified duration.
 void MainMenuScene::fadeOut(int duration) {
-    startButton->hide();
-    settingsButton->hide();
-    exitButton->hide();
-    QPropertyAnimation* animation = new QPropertyAnimation(m_opacityEffect, "opacity");
+    mStartButton->hide();
+    mSettingsButton->hide();
+    mExitButton->hide();
+    QPropertyAnimation* animation = new QPropertyAnimation(mOpacityEffect, "opacity");
     animation->setDuration(duration);
     animation->setStartValue(1.0);
     animation->setEndValue(0.0);
     animation->start(QAbstractAnimation::DeleteWhenStopped);
+}
+
+// Helper function to setup a QPushButton with common properties.
+QPushButton* MainMenuScene::setupButton(const QString& buttonText, QGraphicsLinearLayout* layout) {
+    QPushButton* button = new QPushButton(buttonText);
+    button->setStyleSheet(R"(
+        QPushButton {
+            font-size:20px;
+            font-weight:bold;
+            background:none;
+            background-color:transparent;
+            border-image: url(Resources/img/button.png) 0 0 0 0 stretch stretch;
+        }
+        QPushButton:hover {
+            color:white;
+        }
+        QPushButton:pressed {
+            color:black;
+        }
+    )");
+
+    button->setMinimumSize(400, 120);
+    button->setMaximumSize(400, 120);
+    QGraphicsProxyWidget* buttonProxy = addWidget(button);
+    layout->addItem(buttonProxy);
+    layout->setAlignment(buttonProxy, Qt::AlignCenter);
+
+    return button;
 }
--- Dungeon/mainMenuScene.h
+++ Dungeon/mainMenuScene.h
@@ -1,36 +1,91 @@
+/**
+ * @file mainMenuScene.h
+ * @author Jack Lee
+ * @date 2023.06.22
+ *
+ * @brief This file declares the MainMenuScene class, which represents the main menu of the game.
+ */
+
 #pragma once
 
 #include <QGraphicsScene>
 #include <QSlider>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsOpacityEffect>
+#include <QGraphicsLinearLayout>
 #include <QPushButton>
-
 
 class MainMenuScene : public QGraphicsScene
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief Constructor for the MainMenuScene class.
+     * @param parent The parent QObject that owns this MainMenuScene.
+     */
     MainMenuScene(QObject* parent = nullptr);
+
+    /**
+     * @brief Set the image of the main menu scene.
+     * @param kSceneImg The image to set.
+     */
     void setSceneImg(const QString& kSceneImg);
+
+    /**
+     * @brief Create a fade in animation over the specified duration.
+     * @param duration The duration of the animation in milliseconds.
+     */
     void fadeIn(int duration);
+
+    /**
+     * @brief Create a fade out animation over the specified duration.
+     * @param duration The duration of the animation in milliseconds.
+     */
     void fadeOut(int duration);
 
-
 private:
-    QGraphicsPixmapItem* m_backgroundItem;
-    QGraphicsOpacityEffect* m_opacityEffect;
+    QGraphicsPixmapItem* mBackgroundItem;  // The background image of the scene.
+    QGraphicsOpacityEffect* mOpacityEffect;  // The opacity effect used for fade in/out.
 
+    QPushButton* mStartButton;  // The start button.
+    QPushButton* mSettingsButton;  // The settings button.
+    QPushButton* mLoadingButton;  // The loading button.
+    QPushButton* mExitButton;  // The exit button.
+    QGraphicsPixmapItem* mTitle;  // The title of the main menu.
 
-    QPushButton* startButton;
-    QPushButton* settingsButton;
-    QPushButton* loadingButton;
-    QPushButton* exitButton;
-    QGraphicsPixmapItem* m_title;
+    /**
+     * @brief Set up a QPushButton with the given text, style and add it to the specified layout.
+     *
+     * This function creates a QPushButton, sets up its style including font, background, and color,
+     * sizes it, and then adds it into a QGraphicsLinearLayout. The button is also set to be center-aligned
+     * in the layout.
+     *
+     * @param buttonText The text that will be displayed on the button.
+     * @param layout The QGraphicsLinearLayout that the button will be added to.
+     * @return A pointer to the created QPushButton.
+     */
+    QPushButton* MainMenuScene::setupButton(const QString& buttonText, QGraphicsLinearLayout* layout);
 
 private slots:
+
+    /**
+     * @brief Handle the event when the start button is clicked.
+     */
     void handleStartButton();
+
+    /**
+     * @brief Handle the event when the loading button is clicked.
+     */
     void handleLoadingButton();
+
+    /**
+     * @brief Handle the event when the settings button is clicked.
+     */
     void handleSettingsButton();
+
+    /**
+     * @brief Handle the event when the exit button is clicked.
+     */
     void handleExitButton();
 };
